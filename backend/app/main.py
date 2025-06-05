75918d235fcc22c354008eb474d0af67c98b8e64
Ram nathawat <nathawatr7@gmail.com>
2025-06-05 19:53:07 +0530

    updated again

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust this to your frontend URL in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
