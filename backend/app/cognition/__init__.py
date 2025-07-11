e49b9e567440d7dc40cfb824a8f93e96756d2e4c
Ram nathawat <nathawatr7@gmail.com>
2025-07-11 20:22:21 +0530

    small correction

--- backend/app/cognition/__init__.py
+++ backend/app/cognition/__init__.py
@@ -4,10 +4,14 @@ from . import belief_engine
 from . import belief_evolution
 from . import belief_model
 from . import curiosity_engine
-from . import dream_mode
 from . import goal_manager
 from . import internal_monologue
 from . import knowledge_graph_engine
-from .inference_engine import inference_engine
+
+# Import agents that don't have circular dependencies first
 from .thinker_agent import thinker_agent
+from .inference_engine import inference_engine
+
+# Import agents that depend on the above
 from .orchestrator import orchestrator_agent
+from . import dream_mode # Dream mode might depend on other agents
